6413d2c0448dbe865d2cc5831df0747c660e5581
Ola Klingberg <ola@olaklingberg.com>
2016-10-20 19:05:22 -0400

    Adds custom message event.

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,7 @@ socket.on('connect', function () {
     console.log('Connected to socket.io server!');
 });
 
+socket.on('message', function (message) {
+     console.log('New message:');
+     console.log(message.text);
+});
